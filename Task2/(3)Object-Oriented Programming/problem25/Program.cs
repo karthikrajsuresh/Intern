c07fff4d490f5cc9281b6036ff45e9240da768bf
karthikrajsuresh <114243009+karthikrajsuresh@users.noreply.github.com>
2024-05-27 14:54:42 +0530

    Add files via upload

---
+++ Task2/(3)Object-Oriented Programming/problem25/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ShapeAreaExample
+{
+    abstract class Shape
+    {
+        public abstract double Area();
+    }
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem25_AbstractShapeClass();
+        }
+        static void Problem25_AbstractShapeClass()
+        {
+            Shape circle = new Circle(5.0);
+            Shape rectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine($"Area of the circle: {circle.Area()}");
+            Console.WriteLine($"Area of the rectangle: {rectangle.Area()}");
+        }
+    }
+}
