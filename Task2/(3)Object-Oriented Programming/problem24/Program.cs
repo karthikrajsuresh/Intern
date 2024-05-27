d37be61c17edccd0235211f12ad923733ca8c8f5
karthikrajsuresh <114243009+karthikrajsuresh@users.noreply.github.com>
2024-05-27 14:53:30 +0530

    Add files via upload

---
+++ Task2/(3)Object-Oriented Programming/problem24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Problem24_Encapsulation();
+    }
+    static void Problem24_Encapsulation()
+    {
+        Person person = new Person();
+        person.Name = "nick";
+        person.Age = 26;
+        person.DisplayPersonDetails();
+    }
+}
+public class Person
+{
+    private string name = string.Empty;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value >= 0)
+            {
+                age = value;
+            }
+            else
+            {
+                throw new ArgumentException("Age cannot be negative");
+            }
+        }
+    }
+
+    public void DisplayPersonDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
