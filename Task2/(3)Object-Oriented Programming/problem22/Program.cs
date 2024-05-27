2aa6da44f7136d8841c6e89bec54f6b66929e92d
karthikrajsuresh <114243009+karthikrajsuresh@users.noreply.github.com>
2024-05-27 14:45:18 +0530

    Add files via upload

---
+++ Task2/(3)Object-Oriented Programming/problem22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating.");
+    }
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal is making a sound.");
+    }
+}
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog is barking.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {        Animal animal = new Animal();
+        Console.WriteLine("Animal methods:");
+        animal.MakeSound();
+        animal.Eat();
+        Console.WriteLine();
+        Dog dog = new Dog();
+        Console.WriteLine("Dog methods:");
+        dog.MakeSound();
+        dog.Eat();
+    }
+}
