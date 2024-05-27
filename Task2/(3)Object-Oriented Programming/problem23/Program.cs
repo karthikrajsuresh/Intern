6e6a6a60605e096e1170816d2ff3508eae2c7869
karthikrajsuresh <114243009+karthikrajsuresh@users.noreply.github.com>
2024-05-27 14:52:17 +0530

    Add files via upload

---
+++ Task2/(3)Object-Oriented Programming/problem23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Problem23_PolymorphismWithSpeakMethod();
+    }
+    static void Problem23_PolymorphismWithSpeakMethod()
+    {
+        Animal animal = new Animal();
+        animal.Speak();
+        Dog dog = new Dog();
+        dog.Speak();
+        Animal someAnimal = new Dog();
+        someAnimal.Speak();
+    }
+}
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal speaks");
+    }
+}
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
