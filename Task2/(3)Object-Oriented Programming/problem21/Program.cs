d10ee67104331a4e321e8dbf59e2b2c67973fb5c
karthikrajsuresh <114243009+karthikrajsuresh@users.noreply.github.com>
2024-05-27 14:41:03 +0530

    Add files via upload

---
+++ Task2/(3)Object-Oriented Programming/problem21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Problem21_PersonClass();
+    }
+    static void Problem21_PersonClass()
+    {
+        Person person = new Person("Ram", 25);
+        person.DisplayDetails();
+    }
+}
+class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
